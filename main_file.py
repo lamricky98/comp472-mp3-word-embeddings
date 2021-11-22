3035376235cff5e37aff2412b7791be766acfda4
Ricky Lam <50389025+lamricky98@users.noreply.github.com>
2021-11-21 22:16:08 -0500

    python code finished and generated files are finalized

---
+++ main_file.py
@@ -0,0 +1,138 @@
+import csv
+import json
+
+import pandas as pd
+import gensim.downloader as api
+
+if __name__ == '__main__':
+    pd.set_option('display.max_columns', None)
+
+    synom = pd.read_csv("synonyms.csv")
+
+    csvLines = synom.iloc[:, 0:].values
+    questionList = []
+
+    # Appending question words into a new list
+    for line in csvLines:
+        questionList.append(line[0])
+            
+    answerList = []
+    for line in csvLines:
+        answerList.append(line[1])
+        
+    choice1List = []
+    for line in csvLines:
+        choice1List.append(line[2])
+    
+    choice2List = []
+    for line in csvLines:
+        choice2List.append(line[3])
+    
+    choice3List = []
+    for line in csvLines:
+        choice3List.append(line[4])
+    
+    choice4List = []
+    for line in csvLines:
+        choice4List.append(line[5])
+
+    model_to_use = "glove-twitter-200"
+
+    model = api.load(model_to_use)
+
+    f = open('{}-details.csv'.format(model_to_use), 'w', newline='')
+    # create the csv writer
+    writer = csv.writer(f)
+
+    correct_C = 0
+    no_guess_V = 0
+    for i in range(len(questionList)):
+        counter = 0
+        cosineList = []
+        most_similar_key = ""
+        answerCorrect = ""
+
+        try:
+            # Check the "most similar words", using the default "cosine similarity" measure.
+            result = model.most_similar(questionList[i])
+            most_similar_key, similarity = result[0]  # look at the first match
+        except KeyError:
+            answerCorrect = "guess"
+            most_similar_key = "question-word not found in model"
+            print("{},{},{},{}".format(questionList[i], answerList[i], most_similar_key, answerCorrect))
+            data = [questionList[i], answerList[i], most_similar_key, answerCorrect]
+            writer.writerow(data)
+            continue
+
+        try:
+            cosineList.append(model.similarity(questionList[i], choice1List[i]))
+        except Exception:
+            counter += 1
+            cosineList.append(0)
+
+        try:
+            cosineList.append(model.similarity(questionList[i], choice2List[i]))
+        except Exception:
+            counter += 1
+            cosineList.append(0)
+
+        try:
+            cosineList.append(model.similarity(questionList[i], choice3List[i]))
+        except Exception:
+            counter += 1
+            cosineList.append(0)
+
+        try:
+            cosineList.append(model.similarity(questionList[i], choice4List[i]))
+        except Exception:
+            counter += 1
+            cosineList.append(0)
+
+        if counter == 4:
+            answerCorrect = "guess"
+            most_similar_key = "guess-words not found in model"
+            print("{},{},{},{}".format(questionList[i], answerList[i], most_similar_key, answerCorrect))
+            data = [questionList[i], answerList[i], most_similar_key, answerCorrect]
+            writer.writerow(data)
+            continue
+
+        max_value = max(cosineList)
+
+        max_index = cosineList.index(max_value)
+
+        if max_index == 0:
+            most_similar_key = choice1List[i]
+        elif max_index == 1:
+            most_similar_key = choice2List[i]
+        elif max_index == 2:
+            most_similar_key = choice3List[i]
+        elif max_index == 3:
+            most_similar_key = choice4List[i]
+
+        if (most_similar_key == answerList[i]):
+            answerCorrect = "correct"
+            correct_C += 1
+            no_guess_V += 1
+        elif (most_similar_key == choice1List[i] or most_similar_key == choice2List[i] or most_similar_key ==
+              choice3List[i] or most_similar_key == choice4List[i] and most_similar_key != answerList[i]):
+            answerCorrect = "wrong"
+            no_guess_V += 1
+        print("{},{},{},{}".format(questionList[i], answerList[i], most_similar_key, answerCorrect))
+
+        data = [questionList[i], answerList[i], most_similar_key, answerCorrect]
+        writer.writerow(data)
+
+    model_info = api.info(model_to_use)
+    vocab_len = model_info["num_records"]
+
+    accuracy = correct_C/no_guess_V
+    analysis_f = open('analysis.csv', 'a+', newline='')
+    # create the csv writer
+    analysis_writer = csv.writer(analysis_f)
+
+    data = [model_to_use, vocab_len, correct_C, no_guess_V, accuracy]
+    analysis_writer.writerow(data)
+
+
+
+
